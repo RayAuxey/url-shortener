0b407bdda8b54d254040d3e319aba4dd0f759b3f
RayAuxey <rayauxey@gmail.com>
2024-05-01 16:39:29 +0300

    Add database initialization script to create links table

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello via Bun!");
+import { Database } from "bun:sqlite";
+import { z, type ZodError } from "zod";
+
+const db = new Database("urls.sqlite");
+
+await db.exec(`
+  CREATE TABLE IF NOT EXISTS links (
+      id VARCHAR(10) PRIMARY KEY,
+      url VARCHAR(255) NOT NULL
+  );
+`);
